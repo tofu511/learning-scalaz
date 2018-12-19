9a78e510d2cd915b25a2490af8e6f65f749e1d13
Tomoki Furuhashi <tomoki.furuhashi@bizreach.co.jp>
2018-12-19 17:20:35 +0900

    add Monoid object

--- src/main/scala/day0/IntMonoid.scala
+++ src/main/scala/day0/IntMonoid.scala
@@ -8,10 +8,27 @@ object IntMonoid extends Monoid[Int] {
     def sum1(xs: List[Int]): Int = xs.foldLeft(mzero)(mappend)
     def sum2[A](xs: List[A], m: Monoid[A]): A = xs.foldLeft(m.mzero)(m.mappend)
 
+    implicit val intMonoid = IntMonoid
+    def sum3[A](xs: List[A])(implicit m: Monoid[A]): A = xs.foldLeft(m.mzero)(m.mappend)
+
+    def sum4[A: Monoid](xs: List[A]): A = {
+      val m = implicitly[Monoid[A]]
+      xs.foldLeft(m.mzero)(m.mappend)
+    }
+
+    val res = Monoid.sum(List("a", "b", "c"))
+    assert(res == "abc")
+
     val res1 = sum1(List(1,2,3,4))
     assert(res1 == 10)
 
     val res2 = sum2(List(1,2,3,4), IntMonoid)
     assert(res2 == 10)
+
+    val res3 = sum3(List(1,2,3,4))
+    assert(res3 == 10)
+
+    val res4 = sum4(List(1,2,3,4))
+    assert(res4 == 10)
   }
 }
--- src/main/scala/day0/Monoid.scala
+++ src/main/scala/day0/Monoid.scala
@@ -4,3 +4,22 @@ trait Monoid[A] {
   def mappend(a1: A, a2: A): A
   def mzero: A
 }
+
+object Monoid {
+  implicit val IntMonoid: Monoid[Int] = new Monoid[Int] {
+    override def mappend(a1: Int, a2: Int): Int = a1 + a2
+
+    override def mzero: Int = 0
+  }
+
+  implicit val StringMonoid: Monoid[String] = new Monoid[String] {
+    override def mappend(a1: String, a2: String): String = a1 + a2
+
+    override def mzero: String = ""
+  }
+
+  def sum[A: Monoid](xs: List[A]): A = {
+    val m = implicitly[Monoid[A]]
+    xs.foldLeft(m.mzero)(m.mappend)
+  }
+}
