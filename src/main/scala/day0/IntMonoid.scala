635a6ace8d3f8960cf6b778727a16a6e18fc2b80
Tomoki Furuhashi <tomoki.furuhashi@bizreach.co.jp>
2018-12-19 17:11:28 +0900

    add sum2 function

--- src/main/scala/day0/IntMonoid.scala
+++ src/main/scala/day0/IntMonoid.scala
@@ -1,11 +1,17 @@
+package day0
+
 object IntMonoid extends Monoid[Int] {
   def mappend(a: Int, b: Int): Int = a + b
   def mzero: Int = 0
 
   def main(args: Array[String]): Unit = {
-    def sum(xs: List[Int]): Int = xs.foldLeft(mzero)(mappend)
+    def sum1(xs: List[Int]): Int = xs.foldLeft(mzero)(mappend)
+    def sum2[A](xs: List[A], m: Monoid[A]): A = xs.foldLeft(m.mzero)(m.mappend)
 
-    val res = sum(List(1,2,3,4))
-    assert(res == 10)
+    val res1 = sum1(List(1,2,3,4))
+    assert(res1 == 10)
+
+    val res2 = sum2(List(1,2,3,4), IntMonoid)
+    assert(res2 == 10)
   }
 }
--- src/main/scala/day0/Monoid.scala
+++ src/main/scala/day0/Monoid.scala
@@ -1,3 +1,5 @@
+package day0
+
 trait Monoid[A] {
   def mappend(a1: A, a2: A): A
   def mzero: A
